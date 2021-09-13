3b4d09e7baf1b4411f29799d5c955db5b72ba73f
devgupta2607 <devgupta2607@gmail.com>
2021-09-13 17:12:26 +0530

    Added Answer functionality and responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import HomeIcon from '@material-ui/icons/Home'
 import FeaturedPlayListOutlinedIcon from '@material-ui/icons/FeaturedPlayListOutlined'
 import AssignmentTurnedInOutlinedIcon from '@material-ui/icons/AssignmentTurnedInOutlined'
@@ -7,13 +7,36 @@ import NotificationsOutlinedIcon from '@material-ui/icons/NotificationsOutlined'
 import LanguageIcon from '@material-ui/icons/Language'
 import SearchIcon from '@material-ui/icons/Search'
 import "../css/Navbar.css"
-import { Avatar, Button } from '@material-ui/core'
+import { Avatar, Button, Input } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../features/userSlice'
-import { auth } from '../firebase'
+import db, { auth } from '../firebase'
+import {ExpandMore, Link} from "@material-ui/icons";
+import Modal from 'react-modal'
+import firebase from 'firebase/compat/app';
+
 function Navbar() {
 
     const user = useSelector(selectUser);
+    const [openModal, setOpenModal] = useState(false);
+    const [input, setInput] = useState("");
+    const [inputUrl, setInputUrl] = useState("");
+
+    const handleQuestion = (e) => {
+        e.preventDefault();
+        setOpenModal(false);
+
+        db.collection('questions').add({
+            question: input,
+            imageUrl: inputUrl,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            answer_count: 0,
+            user: user,
+        });
+
+        setInput("");
+        setInputUrl("");
+    }
     return (
         <div className="qHeader">
             <div className="qHeader_logo">
@@ -45,7 +68,51 @@ function Navbar() {
                     <Avatar onClick = {() => auth.signOut()} src = {user.photo} />
                 </div>
                 <LanguageIcon />
-                <Button>Add Question</Button>
+                <Button onClick = {() => setOpenModal(true)}>Add Question</Button>
+                <Modal
+                    isOpen = {openModal}
+                    onRequestClose = {() => setOpenModal(false)}
+                    shouldCloseOnOverlayClick={false}
+                    style = {{
+                        overlay: {
+                            width: 700,
+                            height: 600,
+                            backgroundColor: "rgba(0,0,0,0.8)",
+                            zIndex: "1000",
+                            top: "50%",
+                            left: "50%",
+                            marginTop: "-300px",
+                            marginLeft: "-350px",
+                            padding: "10px 20px 10px 20px"
+                        }
+                    }}
+                >
+                    <div className="modal_title">
+                        <h5>Add Question</h5>
+                        <h5>Share Link</h5>
+                    </div>
+                    <div className="modal_info">
+                        <Avatar className="avatar" src={user.photo} />
+                        <p>{user.displayName ? user.displayName:user.email} asked</p>
+                        <div className = "modal_scope">
+                            <PeopleAltOutlinedIcon />
+                            <p>Public</p>
+                            <ExpandMore />
+                        </div>
+                    </div>
+                    <div className="modal_Field">
+                        <Input required value = {input} onChange = {(e) => setInput(e.target.value)} type= "text" placeholder="Start your question with 'What', 'How', 'Why' etc.'"/>
+                        <div className="modal_fieldLink">
+                            <Link />
+                            <input type= "text" value = {inputUrl} onChange = {(e) => setInputUrl(e.target.value)} placeholder="Optional: include a link that gives context"/>
+                        </div>
+                    </div>
+                    
+                    <div className="modal_buttons">
+                        <button onClick={() => setOpenModal(false)} className="cancel">Close</button>
+                        <button type="submit" onClick = {handleQuestion} className="add">Add Question</button>
+                    </div>
+                </Modal>
             </div>
         </div>
     )
--- src/components/Post.js
+++ src/components/Post.js
@@ -4,27 +4,135 @@ import ArrowDownwardOutlinedIcon from '@material-ui/icons/ArrowDownwardOutlined'
 import ArrowUpwardOutlinedIcon from '@material-ui/icons/ArrowUpwardOutlined'
 import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined'
 import RepeatOutlinedIcon from '@material-ui/icons/RepeatOutlined'
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../css/Post.css"
+import Modal from 'react-modal'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import {selectQuestionId, selectQuestionName, setQuestionInfo} from "../features/questionSlice"
+import { selectUser } from '../features/userSlice'
+import db from '../firebase'
+import firebase from 'firebase/compat/app';
+//import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-function Post() {
+Modal.setAppElement("#root");
+
+function Post({Id,question,imageUrl, timestamp,ques_user,answer_count}) {
+
+    const user = useSelector(selectUser);
+    const [modalOpen,setModalOpen] = useState(false);
+    const dispatch = useDispatch();
+
+    const questionId = useSelector(selectQuestionId);
+    const questionName = useSelector(selectQuestionName);
+    const [answer, setAnswer] = useState("");
+    const [getAnswer, setGetAnswer] = useState([]);
+    const [checkAnswer, setcheckAnswer] = useState(false);
+    const [answerCount, setAnswerCount] = useState(answer_count);
+    const increment = firebase.firestore.FieldValue.increment(1);
+
+    useEffect(() => {
+        if (questionId) {
+            db.collection('questions').doc(questionId).collection('answer')
+            .orderBy('timestamp', 'desc').onSnapshot 
+            (snapshot => setGetAnswer(snapshot.docs.map((doc) => ({
+                id : doc.id,
+                answers: doc.data()
+            }))
+            )
+            );
+            setcheckAnswer(true);
+        }
+    }, [questionId]);
+
+    const handleAnswer = async (e) => {
+        e.preventDefault();
+
+        if (questionId) {
+            db.collection('questions').doc(questionId).collection('answer').add({
+                answer: answer,
+                user: user,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                questionId: questionId,
+            });
+            setAnswerCount(answerCount+1);
+            const res = await db.collection('questions').doc(questionId).update({
+                answer_count: increment
+            }).then(() => {setAnswerCount(answerCount+1)});
+            console.log(questionId, questionName);
+            console.log(res);
+            setAnswer("");
+            setModalOpen(false);
+        }
+    }
     return (
-        <div className="post">
+        <div className="post" onClick = {() =>
+            {
+                if (checkAnswer === false)
+                {
+                    dispatch(setQuestionInfo({
+                        questionId: Id,
+                        questionName: question
+                    }));
+                }
+                else {
+                    setcheckAnswer(false);
+                }
+            } 
+        }
+        >
             <div className="post_info">
-                <Avatar />
-                <h5>Email</h5>
-                <small>1/29/2021, 7:29:25 PM</small>
+                <Avatar src={ques_user.photo}/>
+                <h5>{ques_user.displayName ? ques_user.displayName : ques_user.email}</h5>
+                <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
             </div>
             <div className="post_body">
                 <div className="post_question">
-                    <p>Question</p>
-                    <button className="post_btnAnswer">Answer</button>
+                    <p>{question}</p>
+                    <button className="post_btnAnswer" onClick={() => setModalOpen(true)}>Answer</button>
+                    <Modal 
+                        isOpen={modalOpen}
+                        onRequestClose={() => setModalOpen(false)}
+                        shouldCloseOnOverlayClick={false}
+                        style={{
+                            overlay: {
+                              width: 680,
+                              height: 550,
+                              backgroundColor: "rgba(0,0,0,0.8)",
+                              zIndex: "1000",
+                              top: "50%",
+                              left: "50%",
+                              marginTop: "-250px",
+                              marginLeft: "-350px",
+                            },
+                          }}
+                    >
+                        <div className="modal_question">
+                            <h1> {question} </h1> 
+                            <p>
+                                asked by{" "}
+                                <span className="name">
+                                {ques_user.displayName ? ques_user.displayName : ques_user.email}
+                                </span>{" "}
+                                {""}
+                                on{" "}
+                                <span className="name">
+                                    {new Date(timestamp?.toDate()).toLocaleString()}
+                                </span>
+                            </p>
+                        </div>
+                        <div className="modal_answer">
+                            <textarea value={answer} onChange = {(e) => setAnswer(e.target.value)} type="text" placeholder="Enter Your Answer" />
+                        </div>
+                        <div className="modal_button">
+                            <button className="cancel" onClick={() => setModalOpen(false)}>Cancel</button>
+                            <button className="add" type="submit" onClick={handleAnswer}>Add Answer</button>
+                        </div>
+                    </Modal>
                 </div>
-                <div className="post_answer">
-                    <p></p>
-                </div>
-                <img src="	https://qphs.fs.quoracdn.net/main-qimg-d538f5a097a60f58ef696a6d756a8c7b-mzj" alt="" />
+                
+                <img src={imageUrl} alt="" />
             </div>
             <div className="post_footer">
                 <div className="post_footerAction">
@@ -38,6 +146,49 @@ function Post() {
                     <MoreHorizOutlined />
                 </div>
             </div>
+            <div className="post_answer_count"
+                onClick = {() =>
+                    {
+                        if (checkAnswer === false)
+                        {
+                            dispatch(setQuestionInfo({
+                                questionId: Id,
+                                questionName: question
+                            }));
+                        }
+                        else {
+                            setcheckAnswer(false);
+                        }
+                    } 
+                }
+            >
+                {answerCount} Answers
+            </div>
+            {checkAnswer &&
+                    <div className="post_answers">
+                        {
+                            getAnswer.map(({id,answers}) => (
+                                <div key={id} className="post_answer">
+                                    {
+                                        Id === answers.questionId ? (
+                                            <div className="post_ans_text">
+                                                <div className="txt">{answers.answer}</div>
+                                            
+                                                <div className="ans_info">
+                                                        {answers.user.displayName ? answers.user.displayName : answers.user.email}{" "}
+                                                        on {" "}
+                                                        {new Date(answers.timestamp?.toDate()).toLocaleString()}
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            ""
+                                        )
+                                    }
+                                </div>
+                            ))
+                        }
+                    </div>
+            }
         </div>
     )
 }
